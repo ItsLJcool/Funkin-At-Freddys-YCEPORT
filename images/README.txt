5aafb3f5e52903976985255e4c0c5978978b7658
Landon Shields <lchammer0701@gmail.com>
2022-10-05 22:21:07 -0700

    Shove Images because yes

--- images/README.txt
+++
@@ -1,5 +0,0 @@
-== DROP YOUR IMAGES AND ATLASES HERE ==
-
-You can access them using Paths.getSparrowAtlas and Paths.image
-
-For example, Paths.image("test") will return the image located at "images/test.png"
---
+++ ui/MainMenuState.hx
@@ -0,0 +1,15 @@
+//a
+
+function create() {
+}
+
+function createPost() {
+    
+    for (i in state.menuItems) {
+        state.menuItems.remove(i);
+    }
+
+    state.bg.scale.set(1,1);
+    state.bg.scrollFactor.set();
+
+}
